8ccdf7cff5cc0064631c568371787638e34cd981
cyp <supercyp971@gmail.com>
2025-04-10 10:45:58 +0200

    feat: use migrations with pocketbase

--- main.go
+++ main.go
@@ -7,10 +7,17 @@ import (
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+
+	_ "github.com/carbon-a-rat/death-star-command-center/migrations"
 )
 
 func main() {
 	app := pocketbase.New()
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		Automigrate: true,
+	})
 
 	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
 		// serves static files from the provided public dir (if exists)
