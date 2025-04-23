18c9cdb9719197bec79119bf5f99b4d963265c05
Clément Boittin <clement.boittin@proton.me>
2025-04-23 18:02:43 +0200

    feat: add error and command fields for air and water loading data

---
+++ migrations/1745424109_updated_water_loading_data.go
@@ -0,0 +1,62 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1758497698")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(4, []byte(`{
+			"hidden": false,
+			"id": "number1574812785",
+			"max": null,
+			"min": null,
+			"name": "error",
+			"onlyInt": false,
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "number"
+		}`)); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(5, []byte(`{
+			"hidden": false,
+			"id": "number2395663060",
+			"max": null,
+			"min": null,
+			"name": "command",
+			"onlyInt": false,
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "number"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1758497698")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("number1574812785")
+
+		// remove field
+		collection.Fields.RemoveById("number2395663060")
+
+		return app.Save(collection)
+	})
+}
