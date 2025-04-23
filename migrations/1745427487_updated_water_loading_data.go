de4ad61de761ea297516401bd7b07044a48942e6
Clément Boittin <clement.boittin@proton.me>
2025-04-23 18:58:47 +0200

    refcator: minimal permissions for launchers

---
+++ migrations/1745427487_updated_water_loading_data.go
@@ -0,0 +1,42 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1758497698")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"deleteRule": null,
+			"updateRule": null
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1758497698")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"deleteRule": "@request.auth.id = launch.launcher.id",
+			"updateRule": "@request.auth.id = launch.launcher.id"
+		}`), &collection); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
