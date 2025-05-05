b06fe02c2c025fc8f54a4b6c40395a67c2ba19ac
Clément Boittin <clement.boittin@proton.me>
2025-05-05 12:54:18 +0200

    feat: make drag_coefficient nullable

---
+++ migrations/1746442426_updated_rockets.go
@@ -0,0 +1,56 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1889285177")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(8, []byte(`{
+			"hidden": false,
+			"id": "number3610066113",
+			"max": null,
+			"min": 0,
+			"name": "drag_coefficient",
+			"onlyInt": false,
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "number"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_1889285177")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(8, []byte(`{
+			"hidden": false,
+			"id": "number3610066113",
+			"max": null,
+			"min": 0,
+			"name": "drag_coefficient",
+			"onlyInt": false,
+			"presentable": false,
+			"required": true,
+			"system": false,
+			"type": "number"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
