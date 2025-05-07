b8f76fe5592724234de5c93413b7432e1f2fa972
Clément Boittin <clement.boittin@proton.me>
2025-05-07 15:03:33 +0200

    feat: add migration for codename field in launcher collection

---
+++ migrations/1746623000_updated_launchers.go
@@ -0,0 +1,60 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2089973043")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(1, []byte(`{
+			"autogeneratePattern": "",
+			"hidden": false,
+			"id": "text4217917526",
+			"max": 0,
+			"min": 3,
+			"name": "codename",
+			"pattern": "[a-zA-Z]+(?:_[a-zA-Z]+)*",
+			"presentable": false,
+			"primaryKey": false,
+			"required": true,
+			"system": false,
+			"type": "text"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2089973043")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(1, []byte(`{
+			"autogeneratePattern": "",
+			"hidden": false,
+			"id": "text4217917526",
+			"max": 0,
+			"min": 3,
+			"name": "codename",
+			"pattern": "",
+			"presentable": false,
+			"primaryKey": false,
+			"required": true,
+			"system": false,
+			"type": "text"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
