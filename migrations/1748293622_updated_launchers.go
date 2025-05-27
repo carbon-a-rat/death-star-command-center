94622240f2c93bd578eae7cd0e0051e36cdd6881
Clément Boittin <clement.boittin@proton.me>
2025-05-27 10:13:10 +0200

    feat: add migration to include last_pressure, last_water_flow, last_acc, and last_altitude fields

---
+++ migrations/1748293622_updated_launchers.go
@@ -0,0 +1,100 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2089973043")
+		if err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(18, []byte(`{
+			"hidden": false,
+			"id": "number667870713",
+			"max": null,
+			"min": null,
+			"name": "last_pressure",
+			"onlyInt": false,
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "number"
+		}`)); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(19, []byte(`{
+			"hidden": false,
+			"id": "number734660622",
+			"max": null,
+			"min": null,
+			"name": "last_water_flow",
+			"onlyInt": false,
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "number"
+		}`)); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(20, []byte(`{
+			"hidden": false,
+			"id": "number240362185",
+			"max": null,
+			"min": null,
+			"name": "last_acc",
+			"onlyInt": false,
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "number"
+		}`)); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(21, []byte(`{
+			"hidden": false,
+			"id": "number3282821292",
+			"max": null,
+			"min": null,
+			"name": "last_altitude",
+			"onlyInt": false,
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "number"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_2089973043")
+		if err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("number667870713")
+
+		// remove field
+		collection.Fields.RemoveById("number734660622")
+
+		// remove field
+		collection.Fields.RemoveById("number240362185")
+
+		// remove field
+		collection.Fields.RemoveById("number3282821292")
+
+		return app.Save(collection)
+	})
+}
